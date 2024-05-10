0c51e2aba42025e8bd5c2d986400e0ae1703486c
Fernando Chicaiza <washopilot@yahoo.com>
2024-05-10 11:04:19 -0500

    add webcam library

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import {
     Text,
     useDisclosure
 } from '@chakra-ui/react'
+import Webcam from 'react-webcam'
 
 function App() {
     const { isOpen, onOpen, onClose } = useDisclosure()
@@ -21,7 +22,7 @@ function App() {
         <Container maxW='container.sm' centerContent>
             <Stack h={'100vh'} justifyContent={'center'} alignItems={'center'}>
                 <Heading color={'blue.400'} fontSize={'5xl'}>
-                    Vite + React
+                    Vite + ReactTS
                 </Heading>
                 <Text fontSize={'5xl'}>DFL-Cam</Text>
                 <Button onClick={onOpen}>Lanzar WebCam</Button>
@@ -30,23 +31,21 @@ function App() {
             <Modal isOpen={isOpen} onClose={onClose}>
                 <ModalOverlay />
                 <ModalContent>
-                    <ModalHeader>Modal Title</ModalHeader>
+                    <ModalHeader sx={{ textDecoration: 'underline' }}>Aquí va el título del Modal</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody>
-                        <Text>
+                        <Text pb={'2'}>
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque quod eius totam ducimus
-                            qui modi. Sit, voluptatibus perferendis. Facilis distinctio quam iure deserunt recusandae
-                            soluta, numquam laboriosam doloremque reiciendis officia. Laudantium vitae sapiente ratione
-                            iure mollitia labore voluptates, dolorum ipsam deserunt unde maxime. Similique mollitia
-                            deserunt possimus.
+                            qui modi. Sit, voluptatibus perferendis.
                         </Text>
+                        <Webcam />
                     </ModalBody>
 
                     <ModalFooter>
                         <Button colorScheme='blue' mr={3} onClick={onClose}>
                             Close
                         </Button>
-                        <Button variant='ghost'>Secondary Action</Button>
+                        <Button colorScheme='red'>Enviar DATA</Button>
                     </ModalFooter>
                 </ModalContent>
             </Modal>
